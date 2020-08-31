500f731227174753679473c0ef792b15b9875eb8
AlexanderAcosta07 <walacostam07@gmail.com>
2020-08-30 19:47:16 -0500

    Se agregó estructura de proyecto.

--- SenaGithab/index.php
+++ SenaGithab/index.php
@@ -6,12 +6,14 @@ and open the template in the editor.
 -->
 <html>
     <head>
+    <head>
+        <title>IntranetV2</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="refresh" content="2; url=vista/Login.php">
     </head>
-    <body>
-        <?php
-        hola
-        ?>
-    </body>
+</head>
+<body>
+    <div>CARGANDO DATOS :V</div>
+</body>
 </html>
