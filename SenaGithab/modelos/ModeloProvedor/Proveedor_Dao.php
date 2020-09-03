32fae8eb030c9600494349ca42dd61d2a99d74da
Camilo2994 <67129358+Camilo2994@users.noreply.github.com>
2020-09-03 15:54:12 -0500

    avance provedores dao

--- SenaGithab/modelos/ModeloProvedor/Proveedor_Dao.php
+++ SenaGithab/modelos/ModeloProvedor/Proveedor_Dao.php
@@ -13,7 +13,7 @@ class Proveedor_Dao extends ConBdMySql {
 
 
         $planConsulta = "SELECT P.provIdProvedores, P.provNombreProvedor, P.provDireccionProvedor, P.provTelefonoProvedor, P.prov_Estado, P.prov_created_at, P.prov_updated_at 
-                  FROM provedores P " ;
+                  FROM provedores P ";
         $registrosLibros = $this->conexion->prepare($planConsulta);
         $registrosLibros->execute(); //Ejecución de la consulta 
 
@@ -28,8 +28,8 @@ class Proveedor_Dao extends ConBdMySql {
 //        print_r("holaaaaaaa");
 //        echo "</pre>";
     }
-    
-        public function Insertarproveedor($IdProvedores,$NombreProvedor,$DireccionProvedor,$TelefonoProvedor) {
+
+    public function Insertarproveedor($IdProvedores, $NombreProvedor, $DireccionProvedor, $TelefonoProvedor) {
 
         $planinsertar = "INSERT INTO `provedores` (`provIdProvedores`, `provNombreProvedor`, `provDireccionProvedor`, `provTelefonoProvedor`, `prov_Estado`, `prov_created_at`, `prov_updated_at`)";
 
@@ -44,13 +44,70 @@ class Proveedor_Dao extends ConBdMySql {
         $this->cierreBd();
         return $listadoRegistrosLibro;
     }
-    
-        public function borrar($provIdProvedores) {
 
-        $planborrar = "DELETE FROM `provedores` WHERE `provedores`.`provIdProvedores`=" .$provIdProvedores." ";
 
-        $registrosLibros = $this->conexion->prepare($planborrar);
-        $registrosLibros->execute(); //Ejecución de la consulta 
-        $this->cierreBd();
+    public function seleccionarId($sId) {//llega como parametro un array con datos a consultar
+        if (!isset($sId)) { //si la consulta no viene con el password (PARA REGISTRARSE)
+            $planConsulta = "SELECT * FROM `provedores` WHERE provIdProvedores = $sId";
+            $listar = $this->conexion->prepare($planConsulta);
+            $listar->execute();
         }
+        $registroEncontrado = array();
+        while ($registro = $listar->fetch(PDO::FETCH_OBJ)) {
+            $registroEncontrado[] = $registro;
+        }
+        return $registroEncontrado;
+    }
+
+    public function actualizarId($registro) {
+        try {
+            $NombreProvedor = $registro['nombre'];
+            $DireccionProvedor = $registro['descripcion'];
+            $TelefonoProvedor = $registro['cantidad'];
+            $inserta = $this->conexion->prepare("UPDATE `provedores` SET `provNombreProvedor`='$NombreProvedor',`provDireccionProvedor`='$DireccionProvedor',`provTelefonoProvedor`='$TelefonoProvedor' WHERE provIdProvedores = '$provIdProvedores' ");
+            $inserta->execute();
+            return ['inserto' => 1, 'resultado' => 'Actualizo correctamente'];
+        } catch (Exception $exc) {
+            return ['inserto' => 2, 'resultado' => $exc->getTraceAsString()];
+        }
+    }
+        public function ultimoInsertId() {
+        try {
+            $planConsulta = "SELECT * FROM `provedores` ORDER BY provIdProvedores DESC LIMIT 1";
+            $listar = $this->conexion->prepare($planConsulta);
+            $listar->execute();
+            $registroEncontrado = array();
+            while ($registro = $listar->fetch(PDO::FETCH_OBJ)) {
+                $registroEncontrado[] = $registro;
+            }
+            if (count($registroEncontrado) == 0) {
+                return ['exitoSeleccionId' => 0, 'registroEncontrado' => $registroEncontrado]; /*Pailas */
+            } else {
+                return ['exitoSeleccionId' => 1, 'registroEncontrado' => $registroEncontrado];/* todo bien */
+            }
+        } catch (Exception $exc) {
+            return ['exitoSeleccionId' => 2, 'registroEncontrado' => $exc->getTraceAsString()];
+        }
+    }
+
+    public function EliminadoLogico($Id) {
+        try {
+            $inserta = $this->conexion->prepare("UPDATE `provedores` SET emp_Estado='3' where provIdProvedores = '$Id'");
+            $inserta->execute();
+            return ['inserto' => 1, 'resultado' => 'Actualizo correctamente'];
+        } catch (Exception $exc) {
+            return ['inserto' => 2, 'resultado' => $exc->getTraceAsString()];
+        }
+    }
+    
+    public function Eliminadobd($Id) {
+        try {
+            $inserta = $this->conexion->prepare("DELETE FROM `provedores` WHERE provIdProvedores = '$Id'");
+            $inserta->execute();
+            return ['inserto' => 1, 'resultado' => 'Borro correctamente'];
+        } catch (Exception $exc) {
+            return ['inserto' => 2, 'resultado' => $exc->getTraceAsString()];
+        }
+    }
+
 }
