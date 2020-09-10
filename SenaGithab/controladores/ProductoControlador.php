8463c38bf914860dc0db2f029449be2d5b228404
AlexanderAcosta07 <walacostam07@gmail.com>
2020-09-10 13:56:16 -0500

    Avance ProductoControlador

--- SenaGithab/controladores/ProductoControlador.php
+++ SenaGithab/controladores/ProductoControlador.php
@@ -39,6 +39,34 @@ class ProductoControlador {
                     }
                 }
                 break;
+                
+            case "gestionActualizar":
+                 $gestarActProducto_s = new Producto_Dao(SERVIDOR, BASE, USUARIO_BD, CONTRASENIA_BD); //Se puede llamar de la misma manera gestar?
+
+                $ActualizoProducto_s = $gestarActProducto_s->actualizar($registro);
+                $exitoActualizarProducto_s = $ActualizoProducto_s['inserto'];
+
+                if ($exitoActualizarProducto_s == 1) {
+                    session_start();
+                    $_SESSION['mensaje'] = "Actualizado con èxito";
+                    if ($this->datos['rutaSena'] == 'gestionActualizar') {  //si el formulario de la inserción es el de registrarse y fue exitoso se devuelve a login.php
+                        header("location:Login.php");
+                    }
+                } else {
+                    session_start();
+                    $_SESSION['prodidProducto'] = $this->datos['prodidProducto'];
+                    $_SESSION['prodNombreProducto'] = $this->datos['prodNombreProducto'];
+                    $_SESSION['prodDescripcionProducto'] = $this->datos['prodDescripcionProducto'];
+                    $_SESSION['prodCantidadProducto'] = $this->datos['prodCantidadProducto'];
+                    $_SESSION['prodPrecioNeto'] = $this->datos['prodPrecioNeto'];
+                    $_SESSION['prodPrecioProducto'] = $this->datos['prodPrecioProducto'];
+                    $_SESSION['mensaje'] = "La Atualizacion del producto no se pudo ingresar";
+                    if ($this->datos['rutaSena'] == 'gestionActualizar') {//si al insertar un usuario en el formulario de registrarse y éste ya existe a registro.php
+                        header("location:Login.php");
+                    }
+                }
+                break;
+                
             default :
                 break;
         }
